bc1deaeb4e91a39c8187fe41d99158711f36f492
lpierrot <pierrotlione@seattleu.edu>
2019-10-15 17:14:12 -0700

    removing materialize

--- js/main.js
+++ js/main.js
@@ -11,27 +11,27 @@
 // });
 
 
-$(document).ready(function () {
-        $('.dropdown-button').dropdown({
-            constrainWidth: false,
-            hover: true,
-            belowOrigin: true,
-            alignment: 'left'
-        });
+// $(document).ready(function () {
+//         $('.dropdown-button').dropdown({
+//             constrainWidth: false,
+//             hover: true,
+//             belowOrigin: true,
+//             alignment: 'left'
+//         });
 
-         $('.button-collapse').sideNav({
-             menuWdth: 300,
-             edge: 'left',
-             closeOnClick: false, //close by default
-             draggable: true,
-             onOpen: function () {
-                 console.log('Menu Open')
-             },
-             onClose: function () {
-                 console.log('Menu Close')
-             }
-        });
-    }); 
+//          $('.button-collapse').sideNav({
+//              menuWdth: 300,
+//              edge: 'left',
+//              closeOnClick: false, //close by default
+//              draggable: true,
+//              onOpen: function () {
+//                  console.log('Menu Open')
+//              },
+//              onClose: function () {
+//                  console.log('Menu Close')
+//              }
+//         });
+//     }); 
 // $('.modal').modal({
 //     dismissible: true, // Modal can be dismissed by clicking outside of the modal
 //     opacity: .5, // Opacity of modal background
@@ -39,4 +39,8 @@ $(document).ready(function () {
 //     outDuration: 200, // Transition out duration
 //     startingTop: '4%', // Starting top style attribute
 //     endingTop: '10%', // Ending top style attribute
+// });
+
+// $(document).ready(function () {
+//     $('.scrollspy').scrollSpy();
 // });
